f79290919b69fea4ae9cf63d6a4155762b1518d5
ShiramiSK <81412720+ShiramiSK@users.noreply.github.com>
2024-06-02 01:02:18 +0200

    Added style into the index and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,14 @@
 import React from "react";
-
+import {Link} from 'react-router-dom';
 function Footer(){
     return(
-        <div>
-            Foooter
+        <div className='footer'>
+            <div className='footer-container'>
+                <Link to='https://www.linkedin.com/in/mat%C3%BA%C5%A1-vytyk%C3%A1%C4%8D-03b001212/'>
+                    <i className='fa fa-linkedin-square'/>
+                </Link>
+                <Link to=''/>
+            </div>
         </div>
     );
 }
